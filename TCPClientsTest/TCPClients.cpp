fac7d6b58d6d27730e690fc1514fd0cc8836fbc3
yange <urielyan@sina.com>
2019-03-29 08:39:27 +0800

    打印发送数据给服务器的定时器处理函数所需时间（基本可以忽略不计），目前服务器处理能力取决于客户端发送能力。

--- TCPClientsTest/TCPClients.cpp
+++ TCPClientsTest/TCPClients.cpp
@@ -4,6 +4,7 @@
 #include <QDebug>
 #include <QTimerEvent>
 #include <QtMath>
+#include <QDateTime>
 
 struct TestData
 {
@@ -13,7 +14,8 @@ struct TestData
 
 TCPClients::TCPClients(QObject *parent) : QObject(parent)
 {
-    for (int i = 0; i< 100; ++i)
+
+    for (int i = 0; i< 50; ++i)
     {
         QTcpSocket *socket = new QTcpSocket(this);
         socket->connectToHost(QHostAddress::LocalHost, 11555);
@@ -22,11 +24,12 @@ TCPClients::TCPClients(QObject *parent) : QObject(parent)
 
 
         //启动多个计时器
-        int timeID = startTimer(20);
+        int timeID = startTimer(5);
         m_sockets[timeID] = socket;
         //m_sockets.append(socket);
     }
     //connect()
+
 }
 
 void TCPClients::receiveServerData()
@@ -53,6 +56,11 @@ void TCPClients::disconnectedFromServer()
 
 void TCPClients::timerEvent(QTimerEvent *event)
 {
+    static QDateTime initDateTime = QDateTime::currentDateTime();
+    static int count = 0;
+    QDateTime inFunTime= QDateTime::currentDateTime();
+
+
     int timeID = event->timerId();
     if (!m_sockets.contains(timeID))
     {
@@ -67,8 +75,18 @@ void TCPClients::timerEvent(QTimerEvent *event)
     TestData structData;
     structData.a = qrand() % 10;
     structData.b = qrand() % 10;
-
-
     socket->write(QByteArray((const char*)(&structData), sizeof(structData)));//
 
+
+
+    count++;
+    if (QDateTime::currentDateTime().msecsTo(initDateTime) < -1000)
+    {
+        qDebug() << "one second can solve data " << count;
+        count = 0;
+        initDateTime = QDateTime::currentDateTime();
+    }
+
+    //发送数据的时间基本可以忽略不计
+    //qDebug() << QDateTime::currentDateTime().msecsTo(inFunTime);
 }
